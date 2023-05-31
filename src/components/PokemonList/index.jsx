948c51e458a16cbce0796dd282da9fc9fd6c3f4e
ViniciusJMR <viniciusdejesus605@gmail.com>
2023-05-30 23:54:01 -0300

    fix: fixed list of pokemon

--- src/components/PokemonList/index.jsx
+++ src/components/PokemonList/index.jsx
@@ -4,62 +4,19 @@ import axios from "axios"
 import './style.css'
 import SearchBar from "./SearchBar";
 import { BASE_URL } from "../../utils/request";
-import { type } from "@testing-library/user-event/dist/type";
 
-const pokemon = [
-{ name: "Belgium", continent: "Europe" },
-{ name: "India", continent: "Asia" },
-{ name: "Bolivia", continent: "South America" },
-{ name: "Ghana", continent: "Africa" },
-{ name: "Japan", continent: "Asia" },
-{ name: "Canada", continent: "North America" },
-{ name: "New Zealand", continent: "Australasia" },
-{ name: "Italy", continent: "Europe" },
-{ name: "South Africa", continent: "Africa" },
-{ name: "China", continent: "Asia" },
-{ name: "Paraguay", continent: "South America" },
-{ name: "Usa", continent: "North America" },
-{ name: "France", continent: "Europe" },
-{ name: "Botswana", continent: "Africa" },
-{ name: "Spain", continent: "Europe" },
-{ name: "Senegal", continent: "Africa" },
-{ name: "Brazil", continent: "South America" },
-{ name: "Denmark", continent: "Europe" },
-{ name: "Mexico", continent: "South America" },
-{ name: "Australia", continent: "Australasia" },
-{ name: "Tanzania", continent: "Africa" },
-{ name: "Bangladesh", continent: "Asia" },
-{ name: "Portugal", continent: "Europe" },
-];
 
 function PokemonList(){
         const [searchInput, setSearchInput] = useState("")
         const [apiResp, setApiResp] = useState([])
 
 
-
         useEffect(() => {
-            const {search} = window.location
-            const s = new URLSearchParams(search).get('s')
-            console.log(s)
-
-            if(s === null){
-                axios.get(`${BASE_URL}/pokemon/`)
+                axios.get(`${BASE_URL}/pokemon/?offset=0&limit=151`)
                     .then(response => {
-                        setApiResp(JSON.stringify(response))
-                        console.log(apiResp)
+                        setApiResp(response.data.results)
+                        console.log(response.data.results)
                     })
-            }
-            else{
-                axios.get(`${BASE_URL}/pokemon/${s}`)
-                    .then(response => {
-                        // setApiResp(response.data.content)
-                        setApiResp(JSON.stringify(response))
-                        console.log(apiResp)
-                    })
-
-            }
-
 
         }, [])
 
@@ -71,15 +28,13 @@ function PokemonList(){
             <thead>
                 <tr>
                     <th>Name</th>
-                    <th>Base EXP</th>
                 </tr>
             </thead>
             <tbody >
-            {Object.keys(apiResp).map((data,index) => {
+            {apiResp.map((p) => {
                 return (
-                <tr key={index}>
-                    <td>{data}</td>
-                    <td>{data}</td>
+                <tr>
+                    <td>{p.name}</td>
                 </tr>)
             })}
             </tbody>
