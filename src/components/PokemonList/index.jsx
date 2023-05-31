295eb77acf434c6e11571cfef904aaacfd799211
ViniciusJMR <viniciusdejesus605@gmail.com>
2023-05-31 17:49:33 -0300

    edit:switch table to list on pokemonList & useEffect now calls each pokemon id endpoint

--- src/components/PokemonList/index.jsx
+++ src/components/PokemonList/index.jsx
@@ -1,4 +1,4 @@
-import {useEffect,useState} from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios"
 
 import './style.css'
@@ -6,39 +6,55 @@ import SearchBar from "./SearchBar";
 import { BASE_URL } from "../../utils/request";
 
 
-function PokemonList(){
-        const [searchInput, setSearchInput] = useState("")
-        const [apiResp, setApiResp] = useState([])
+function PokemonList() {
+    // const [searchInput, setSearchInput] = useState("")
+    const [apiResp, setApiResp] = useState([])
+    const dataRef = useRef(false)
+    let pokemons = []
+    let MAX_POKEMON_COUNT = 151
 
+    const getPokemon = async id =>{
+        let res = await axios.get(`${BASE_URL}/pokemon/${id}`)
+        pokemons.push(res.data)
+        setApiResp(pokemons)
+        return res
+    }
 
-        useEffect(() => {
-                axios.get(`${BASE_URL}/pokemon/?offset=0&limit=151`)
-                    .then(response => {
-                        setApiResp(response.data.results)
-                        console.log(response.data.results)
-                    })
+    useEffect(() => {
+        // Evita que o código execute mais de uma vez na desenv
+        //TODO: Analizar uma forma melhor de evitar que useeffect seja executado
+        //      Mais de uma vez na desenv
+        if (dataRef.current) return;
+        dataRef.current = true
 
-        }, [])
+        // for (let id = 1; id <= MAX_POKEMON_COUNT; id++) {
+        //     axios.get(`${BASE_URL}/pokemon/${id}`)
+        //         .then(response => {
+        //             pokemons.push(response.data)
+        //             setApiResp(pokemons)
+        //         })
+        // }
+        
+        const ids = Array.from({length:MAX_POKEMON_COUNT}, (v,k) => k+1)
+        Promise.all(ids.map(id => getPokemon(id)))
+    }, [])
 
 
     return (
         <div className="flex-child">
-            <SearchBar/>
-            <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                </tr>
-            </thead>
-            <tbody >
-            {apiResp.map((p) => {
-                return (
-                <tr>
-                    <td>{p.name}</td>
-                </tr>)
-            })}
-            </tbody>
-            </table>
+            <SearchBar />
+            <ul>
+                {
+                    apiResp.map((p) => {
+                        return (
+                            <li key={p.id}>
+                                {p.id}. {p.name}
+                            </li>
+                        )
+                    })
+                }
+
+            </ul>
         </div>
     )
 }
